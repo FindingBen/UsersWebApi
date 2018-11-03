e9cd78f7f56562bc10f0e4ecb98d8fd4e027578a
Benjamin <33846168+FindingBen@users.noreply.github.com>
2018-11-03 13:18:14 +0100

    Update ConnectionString.cs

--- UsersWebApi/Controllers/ConnectionString.cs
+++ UsersWebApi/Controllers/ConnectionString.cs
@@ -8,6 +8,6 @@ namespace UsersWebApi.Controllers
     public class ConnectionString
     {
         public static readonly string connectionString =
-            "Server=tcp:findingben.database.windows.net,1433;Initial Catalog=benbase;Persist Security Info=False;User ID=ben;Password=Arsenal1997;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            "Server=tcp:findingben.database.windows.net,1433;Initial Catalog=benbase;Persist Security Info=False;User ID=(ur id);Password=(ur pass);MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
