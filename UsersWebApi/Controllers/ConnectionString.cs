e2aeca57d9a6a35abee9fd425cd2f3f2dc361ce0
Benjamin <33846168+FindingBen@users.noreply.github.com>
2018-11-03 13:30:09 +0100

    Update ConnectionString.cs

--- UsersWebApi/Controllers/ConnectionString.cs
+++ UsersWebApi/Controllers/ConnectionString.cs
@@ -7,6 +7,8 @@ namespace UsersWebApi.Controllers
 {
     public class ConnectionString
     {
+        //Remember to delete this con string and use your own ;) 
+        //I forgot how to ignore it from github :D 
         public static readonly string connectionString =
             "Server=tcp:findingben.database.windows.net,1433;Initial Catalog=benbase;Persist Security Info=False;User ID=(ur id);Password=(ur pass);MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
